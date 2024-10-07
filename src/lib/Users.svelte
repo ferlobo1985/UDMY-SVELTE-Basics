fab7a2e22862ddd35a48d49cf508a173dde1af9a
ferlobo1985 <fernando.lobo.prez@gmail.com>
2024-10-07 16:19:34 -0300

    04-The Script tag
    04-The Script tag

--- src/lib/Users.svelte
+++ src/lib/Users.svelte
@@ -1,5 +1,17 @@
-<ul>
-    <li>Mike</li>
-    <li>Lisa</li>
-    <li>Mark</li>
-</ul>
+<script>
+    let name = 'Mark';
+    let color = 'red';
+    function getNumber(){
+        return Math.random()
+    }
+
+    setTimeout(()=>{
+        color = 'blue';
+        name = 'steve'
+    },3000)
+
+</script>
+
+<h2>My name is {name}</h2>
+<p>The number is {getNumber()}</p>
+<p style="color:{color}">The sum is {1+10}</p>
